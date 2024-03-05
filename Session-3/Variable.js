f4c40f3bdd7be3dc899e667d76b413e855f6e4c3
Kabir <sagithedeveloper@gmail.com>
2024-03-05 10:06:46 +0530

    added the code uptodate

---
+++ Session-3/Variable.js
@@ -0,0 +1,13 @@
+var city; // Declaration
+
+city = "Delhi"; // Assignment
+
+var id = 101; // Initialization
+
+id = true;
+city = 111;
+
+function fn() {
+  var username;
+  username = "Kabir";
+}
---
+++ Session-3/function.js
@@ -0,0 +1,11 @@
+function fnAdd() {
+  var x;
+  var y;
+  var result;
+  x = 100;
+  y = 200;
+  result = x + y;
+  console.log(result);
+}
+
+fnAdd();
